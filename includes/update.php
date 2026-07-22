15d99eaef4ceae390de24517cf207cd225d28d5d
Richer Yang <fantasyworldidvtw@gmail.com>
2026-07-22 09:09:59 +0800

    修正檔名大小寫錯誤

--- includes/update.php
+++ includes/update.php
@@ -17,8 +17,8 @@ final class RY_TFTUTOR_Update
             return;
         }
 
-        if (version_compare($now_version, '2026.7.14', '<')) {
-            RY_TFTUTOR::update_option('version', '2026.7.14', true);
+        if (version_compare($now_version, '2026.7.16', '<')) {
+            RY_TFTUTOR::update_option('version', '2026.7.16', true);
         }
     }
 }
